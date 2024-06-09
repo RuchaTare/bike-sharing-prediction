50b6df72ce9b67e66510b53d1cef8c21205f4eaf
Rucha Tare <tarerucha8@gmail.com>
2024-06-09 12:31:34 +0100

    added main.py

---
+++ src/main.py
@@ -0,0 +1,29 @@
+"""
+Main module for the project
+"""
+
+import logging
+import os
+
+from logger import setup_logging
+from preprocessing import preprocessor
+from model_training import trainer
+from utils import read_yaml
+
+
+def main():
+    """
+    Main function
+    """
+    setup_logging()
+    logging.info("Application started")
+
+    config_data = read_yaml("../config.yaml")
+
+    preprocessor(config_data)
+
+    trainer(config_data)
+
+
+if __name__ == "__main__":
+    main()
